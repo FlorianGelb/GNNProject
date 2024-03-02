61141d0628e89df191f377cf4c5ce0dd32c3e0b7
Florian Gelb <irolori64@gmail.com>
2024-03-02 20:42:44 +0100

    batched

--- Pruningmethods/ShapSAE.py
+++ Pruningmethods/ShapSAE.py
@@ -16,91 +16,52 @@ from tqdm import tqdm
 from multiprocessing import Pool
 
 
-def custom_function(model, data, weights, layer, nodes):
+def custom_function(model, data, weights, layer, index_start, index_stop):
+    weight_matrix_shape =  model.encoder[layer].weight.data.shape
+    weight_matrix = model.encoder[layer].weight.data.reshape(1, -1).flatten()
     outputs = []
-    weight_matrix = model.encoder[layer].weight.data.clone()
     for w in weights:
-        for node in nodes:
-            weight_matrix[:, node] = torch.Tensor(w)
-        model.encoder[layer].weight = torch.nn.Parameter(weight_matrix)
+        weight_matrix[index_start:index_stop] = torch.FloatTensor(w)
+        weight = torch.nn.Parameter(weight_matrix.reshape(weight_matrix_shape))
+        model.encoder[layer].weight = weight
         outputs.append(model.calculate_loss(data).detach().numpy())
     return np.array(outputs)
 
 
-def compute_shap_values(args):
-    model, data_set, weights, layer, nodes = args
-    return shap.KernelExplainer(lambda w: custom_function(model, data_set, w, layer, nodes), weights).shap_values(
-        weights)
+def prune(model: AutoEncoder, importance_level, data_set, number_of_batch=2000, background_data_samples=10):
 
-
-def parallel_map_with_progress(pool, func, iterable, total=None):
-    results = []
-    with tqdm(total=total) as pbar:
-        for result in pool.imap(func, iterable):
-            results.append(result)
-            pbar.update()
-    return results
-
-
-def prune(model, importance_level, data_set, background_data_samples=5, batch_size=10, n_jobs=None):
     model = copy.deepcopy(model)
     for layer in tqdm(range(len(model.encoder))):
         if type(model.encoder[layer]) != torch.nn.modules.linear.Linear:
             continue
-        encoder_weight_matrix = model.encoder[layer].weight.data
-        decoder_layer = len(model.decoder) - layer - 1
-        decoder_weight_matrix = model.decoder[decoder_layer].weight.data
+        weights_in_layer = model.encoder[layer].weight.data.reshape(1, -1).flatten()
 
-        # Generate background data for SHAP
-        weights_in_layer = model.encoder[layer].weight.data.flatten()
+        num_batches = len(weights_in_layer) // number_of_batch
+        shapley_values = []
         background_data = np.random.uniform(weights_in_layer.min(), weights_in_layer.max(),
-                                            (background_data_samples, len(weights_in_layer)))
+                                            (background_data_samples, number_of_batch))
+        for i in tqdm(range(num_batches)):
+            index_start, index_stop = i * number_of_batch, (i + 1) * number_of_batch
+            batch_weights = weights_in_layer.detach().numpy()[index_start : index_stop]
+            explainer = shap.KernelExplainer(lambda w: custom_function(model, data_set, w, layer, index_start, index_stop), background_data)
+            shapley_values_badge = abs(explainer.shap_values(batch_weights))
+            shapley_values.append(shapley_values_badge)
 
-        # Parallelize computation of SHAP values for nodes in the layer
-        if n_jobs == -1:
-            processes = None  # Use all available CPU cores
-        else:
-            processes = n_jobs
+        shapley_values = np.array(shapley_values).flatten()
+        layer_importance = sum(shapley_values)
+        sorted_indices = np.flip(np.argsort(shapley_values))
+        cumulative_importance = np.cumsum(shapley_values[sorted_indices])
+        cutoff = np.argmax(cumulative_importance >= layer_importance * importance_level)
 
-        pool = Pool(processes=processes)
+        mask = np.ones(shapley_values.shape, dtype=bool)
+        mask[:] = False
+        mask[sorted_indices[:cutoff]] = True
+        mask = mask.reshape(model.encoder[layer].weight.shape)
 
-        total_nodes = model.encoder[layer].weight.shape[1]
-        num_batches = (total_nodes + batch_size - 1) // batch_size
-
-        results = []
-        for i in range(num_batches):
-            start = i * batch_size
-            end = min((i + 1) * batch_size, total_nodes)
-            nodes_batch = list(range(start, end))
-
-            results.extend(parallel_map_with_progress(pool, compute_shap_values,
-                                                      [(model, data_set, background_data, layer, nodes_batch)]))
-
-        pool.close()
-        pool.join()
-
-        for node, shap_values in enumerate(results):
-            shapley_values = abs(shap_values)
-            layer_importance = sum(shapley_values)
-            sorted_indices = np.flip(np.argsort(shapley_values))
-            cumulative_importance = np.cumsum(shapley_values[sorted_indices])
-            cutoff = np.argmax(cumulative_importance >= layer_importance * importance_level)
-            mask = np.ones_like(shapley_values, dtype=bool)
-            mask[:] = False
-            mask[sorted_indices[:cutoff]] = True
-            mask = mask.reshape(encoder_weight_matrix[:, node].shape)
-            new_weight = torch.nn.Parameter(encoder_weight_matrix[:, node] * mask)
-            encoder_weight_matrix[:, node] = torch.Tensor(new_weight)
-            model.encoder[layer].weight = torch.nn.Parameter(encoder_weight_matrix)
-            new_weight = torch.nn.Parameter(decoder_weight_matrix[node, :] * mask)
-            decoder_weight_matrix[node, :] = torch.Tensor(new_weight)
-            model.decoder[decoder_layer].weight = torch.nn.Parameter(decoder_weight_matrix)
-
-        break
+        new_weight = torch.nn.Parameter(model.encoder[layer].weight.data * mask)
+        model.encoder[layer].weight = new_weight
 
     return model
-
-
 
 """
 def custom_function(model, data, weights, layer, node):
